ddba7e2708e456d762c72da338282641069ddd3d
Phinahas Philip <phinahasphilip2000@gmail.com>
2023-10-18 11:56:24 +0530

    API update: added token verifcation to category routes

--- src/routes/user/general.js
+++ src/routes/user/general.js
@@ -5,8 +5,8 @@ const tokenverification = require('../../middlewares/tokenVerification')
 
 const router = express.Router();
 
-router.post('/create-category',userController.createCategory);
-router.get('/get-categories',userController.getCategories);
+router.post('/create-category',tokenverification.isUser,userController.createCategory);
+router.get('/get-categories',tokenverification.isUser,userController.getCategories);
 router.post('/add-expense',tokenverification.isUser,userController.addExpense);
 router.get('/get-user',tokenverification.isUser,userController.getUser);
 
